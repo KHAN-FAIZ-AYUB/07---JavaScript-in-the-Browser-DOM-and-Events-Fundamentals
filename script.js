7f18f310899d38938f07f43aa1abc8fc26bad6cd
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-08-02 12:35:59 +0530

    EventListner

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContentb);
 document.querySelector('.message').textContent = 'Correct Number!';
 
@@ -8,3 +8,15 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value=23;
 console.log(document.querySelector('.guess').value);
+*/
+
+// ******************** EventListner ********************
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  }
+});
