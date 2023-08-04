b1b4dd16305a22f656c07c23bde0d223e02bcb16
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-08-04 18:27:35 +0530

    challenge #01

--- script.js
+++ script.js
@@ -12,9 +12,8 @@ console.log(document.querySelector('.guess').value);
 
 // ******************** EventListner ********************
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -29,6 +28,7 @@ document.querySelector('.check').addEventListener('click', function () {
   //   whan input is correct
   else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number! 🎉';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -59,4 +59,15 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('body').style.backgroundColor = 'red;';
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start Guessing';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').style.width = '15rem';
 });
